bc02c4cf694c54eb06202554d12c4a4e62a22efc
Navonil Mukherjee <navonil_mukherjee@yahoo.co.in>
2018-01-09 01:53:49 +0530

    Update

--- time01.cpp
+++ time01.cpp
@@ -1,4 +1,4 @@
-//time01.cpp
+//time01.cpp ---- The following code creates three objects of class Time and adds them using operator overloading
 #include <iostream>
 #include "time00.h"
 
@@ -25,7 +25,16 @@ int main()
 	cin>>s;
 	Time t2(h, m, s);
 	t2.display();
-	Time result = t1 + t2;
+	cout<<"Second time object created successfully. Please enter the third and final set of details."<<endl;
+	cout<<"Hours:"<<endl;
+	cin>>h;
+	cout<<"Minutes:"<<endl;
+	cin>>m;
+	cout<<"Seconds:"<<endl;
+	cin>>s;
+	Time t3(h, m, s);
+	t3.display();
+	Time result = t1 + t2 + t3;
 	cout<<"Total time:"<<endl;
 	result.display();
 	return 0;
