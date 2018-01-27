9d8fab4a2eb225350f0010094f47c51b4f2694cc
Navonil Mukherjee <navonil_mukherjee@yahoo.co.in>
2018-01-27 15:51:31 +0530

    Update

--- vector_STL.cpp
+++ vector_STL.cpp
@@ -8,13 +8,9 @@ int main()
 	int s = 0, input = 0;
 	vector<int> val;
 	vector<int>::iterator ptr;
-	cout<<"Please enter as many integers as you like."<<endl;
-	do{
-		cin>>input;
+	cout<<"Please enter as many integers as you like.Enter ESC followed by Return when you're done."<<endl;
+	while(cin>>input)
 		val.push_back(input);
-		cout<<"Continue? (1 = Yes / 0 = No)"<<endl;
-		cin>>input;
-	}while(input != 0);
 	cout<<"You have entered "<<val.size()<<" items."<<endl;
 	cout<<"The elements entered are:"<<endl;
 	for(ptr = val.begin(); ptr != val.end(); ptr++)
