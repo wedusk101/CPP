68f8b082e47eae7a0fbeca0873bab8239801e129
Navonil Mukherjee <navonil_mukherjee@yahoo.co.in>
2018-03-31 16:52:01 +0530

    Intro to ASCII Art

---
+++ ascii.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+#define SIZE 
+
+int main()
+{
+	for(double x = 0; x < 20; x++)
+	{
+		for(double y = 0; y < 20; y++)
+		{
+			if( y == round(sin(x)) + 4) // sin curve
+				std::cout << "*";
+			else
+				std::cout << " ";
+		}
+		std::cout << "\n";
+	}
+}
