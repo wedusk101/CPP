1966b1d2e204e5788017103ff7d4b2404f0ab94f
Navonil Mukherjee <navonil_mukherjee@yahoo.co.in>
2018-06-21 17:49:54 +0530

    Bugs

--- look_n_say_WIP.cpp
+++ look_n_say_WIP.cpp
@@ -25,42 +25,40 @@ int main()
 	std::cout << "Please enter the number of terms to display." << std::endl;
 	std::cin >> terms;
 	int seq[128] = {0};
-	// seq[127] = -1;
-	// display(seq);
 	lookNsay(seq, terms);	
 }
 
 void display(int *arr)
 {
-	for(int i = 0; arr[i] != -1; i++)
+	for(int i = 0; arr[i] != 0; i++)
 		std::cout << arr[i];
 	std::cout << "\n";
 }
 
 void lookNsay(int *arr, int num)
 {
-	int continuousSeqCount = 0, i = 0, j = 0, currentTerm = 0, terms = 0;
+	int continuousSeqCount = 0, i = 0, j = 0, bak = 0, currentTerm = 0, terms = 0;
 	arr[0] = 1;
-	arr[1] = -1;
+	// arr[1] = -1;
 	while(terms != num)
 	{
-		// std::cout << " Here" <<"\n";
 		display(arr);
 		terms++;
-		i = j = continuousSeqCount = 0;
+		i = j = bak = continuousSeqCount = 0;
 		while(arr[i] != 0)
 		{
-			// display(arr);
-			currentTerm = arr[i]; // infinite loop as arr[1] is initially -1
+			currentTerm = arr[i]; 
 			continuousSeqCount = 1; 
-			std::cout << " Current " << currentTerm <<"\n";
+			// std::cout << "Current " << currentTerm <<"\n";
 			while(arr[++i] == currentTerm) // currentTerm is -1 here - infinite loop
 				++continuousSeqCount;
 			arr[j++] = continuousSeqCount;
-			arr[j++] = currentTerm;
-			arr[j] = -1;
-			// std::cout << " Here 59 i j " << i << " " << j <<"\n";
+			arr[j++] = currentTerm; // overwrites existing terms
+			// arr[j] = -1;
+			// k = j + 1;
+			i = j + 1;
+			// std::cout << " Here 66 i j " << i << " " << j <<"\n";
 		}
-		// std::cout << " Here 66" <<"\n";		
+		// std::cout << " Here 68" <<"\n";		
 	}
 }
