b7bafb560d11e33eaa494fe8e92304fa52676a8a
Navonil Mukherjee <navonil_mukherjee@yahoo.co.in>
2018-09-30 20:52:14 +0530

    STL Stack Test

---
+++ stack_STL.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <stack>
+
+int main()
+{
+	std::stack<int> s;
+	int val = 0;
+	while(std::cin >> val)
+		s.push(val);
+	std::cout << "popping..." << std::endl;
+	while(!s.empty())
+	{
+		// val = s.pop();
+		std::cout << s.top() << std::endl;
+		s.pop();
+	}
+}
