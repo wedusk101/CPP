8b5e46127c38054345668a69b3ab1dae3350a96a
Navonil Mukherjee <navonil_mukherjee@yahoo.co.in>
2018-10-10 00:09:45 +0530

    Fisher-Yates Shuffle

--- Knuth_Fisher_Yates_Shuffle_inplace.cpp
+++ Knuth_Fisher_Yates_Shuffle_inplace.cpp
@@ -1,10 +1,10 @@
-/*The following code implements the Knuth-Fisher-Yates linear time shuffling algorithm.*/
+/*The following code implements the in-place version of the Knuth-Fisher-Yates linear time shuffling algorithm.*/
 #include <iostream>
 #include <random>
 #include <ctime>
 
 void swap(int &, int &);
-int* shuffle(int*, int);
+void shuffle(int*, int);
 
 int main()
 {
@@ -22,11 +22,10 @@ int main()
 		std::cin >> ch;
 		if(ch != 's') // shuffles the list every time the user enters s
 			break;
-		int *result = shuffle(input, size);
+		shuffle(input, size);
 		for(int i = 0; i < size; i++)
-			std::cout << result[i] << " ";
+			std::cout << input[i] << " ";
 		std::cout << "\n";
-		delete [] result;
 	}
 	std::cout << "Thank you." << std::endl;
 	delete [] input;
@@ -39,20 +38,16 @@ void swap(int &a, int &b)
 	b = tmp;
 }
 
-int* shuffle(int *input, int size)
+void shuffle(int *input, int size)
 {
-	int range = size - 1, i = 0, j = size - 1, index = 0;
-	int *output = new int[size];
+	int range = size - 1, i = 0, index = 0;
 	std::mt19937 rand(time(0));
 	while(i != size)
 	{
 		std::uniform_int_distribution<int> roll(0, range);
 		index = roll(rand);
-		output[i] = input[index];
-		swap(input[index], input[j]);
+		swap(input[index], input[range]);
 		i++;
 		range--;
-		j--;
 	}
-	return output;
 }
---
+++ Knuth_Fisher_Yates_Shuffle_non_inplace.cpp
@@ -0,0 +1,58 @@
+/*The following code implements the Knuth-Fisher-Yates linear time shuffling algorithm. Note that this is not the in-place implementation and uses O(n) auxiliary memory.*/
+#include <iostream>
+#include <random>
+#include <ctime>
+
+void swap(int &, int &);
+int* shuffle(int*, int);
+
+int main()
+{
+	int size = 0;
+	char ch;
+	std::cout << "Enter the size of the list." << std::endl;
+	std::cin >> size;
+	int *input = new int[size];
+	std::cout << "Enter the numbers." << std::endl;
+	for(int i = 0; i < size; i++)
+		std::cin >> input[i];
+	std::cout<<"Enter s to shuffle the list, q to quit."<< std::endl;
+	while(1)
+	{
+		std::cin >> ch;
+		if(ch != 's') // shuffles the list every time the user enters s
+			break;
+		int *result = shuffle(input, size);
+		for(int i = 0; i < size; i++)
+			std::cout << result[i] << " ";
+		std::cout << "\n";
+		delete [] result;
+	}
+	std::cout << "Thank you." << std::endl;
+	delete [] input;
+}
+
+void swap(int &a, int &b)
+{
+	int tmp = a;
+	a = b;
+	b = tmp;
+}
+
+int* shuffle(int *input, int size)
+{
+	int range = size - 1, i = 0, j = size - 1, index = 0;
+	int *output = new int[size];
+	std::mt19937 rand(time(0));
+	while(i != size)
+	{
+		std::uniform_int_distribution<int> roll(0, range);
+		index = roll(rand);
+		output[i] = input[index];
+		swap(input[index], input[j]);
+		i++;
+		range--;
+		j--;
+	}
+	return output;
+}
