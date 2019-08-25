3f66c85432b155404493951efd006de1c1af9740
Navonil Mukherjee <navonil_mukherjee@yahoo.co.in>
2019-08-25 23:06:15 +0530

    Updated method for random sampling

--- openmp_monte_pi.cpp
+++ openmp_monte_pi.cpp
@@ -1,14 +1,14 @@
 /*The following code calculates the approximate value of Pi using Monte Carlo Sampling.*/ 
 #include <iostream>
-#include <ctime>
-#include <cstdlib>
 #include <iomanip>
 #include <omp.h>
+#include <random>
 
 int main() 
 {
-	srand(time(NULL));
-	long long count = 0, max = 0, samples = 0, x = 0, y = 0; // the value of samples controls the precision of the approximation
+	std::default_random_engine generator;
+	std::uniform_real_distribution<double> rnd(0, 1);
+	long double count = 0, samples = 0; // the value of samples controls the precision of the approximation
 	long double pi = 0.0, start = 0.0, stop = 0.0;
 	int num_of_threads;
 	int nthreads;
@@ -16,20 +16,23 @@ int main()
 	std::cin>>samples;
 	std::cout<<"Please enter the number of threads to spawn."<<std::endl;
 	std::cin>>num_of_threads;
-	max = samples * samples; // square of the radius = total number of samples
+	long double max = samples * samples; // square of the radius = total number of samples
 	omp_set_num_threads(num_of_threads);
 	start = omp_get_wtime();
-	#pragma omp parallel for private(x,y) reduction(+:count)
-	for(long long i = 0; i < max; i++) // sampling
+	#pragma omp parallel reduction(+:count)
 	{	
-		x = rand() % samples + 1;
-		y = rand() % samples + 1;
-			if(x * x + y * y <= max) // x^2 + y^2 <= r^2 where r is the radius
+		#pragma omp for 
+		for(long long i = 0; i < (long long)max; i += 1) // sampling
+		{	
+			long double x = rnd(generator) * samples;
+			long double y = rnd(generator) * samples;
+			long double dist = x * x + y * y;
+			if(dist <= max) // x^2 + y^2 <= r^2 where r is the radius
 				count++;		
+		}
 	}
-	//nthreads = omp_get_num_threads();
-	pi = (4 * count)/(long double)max;
 	stop = omp_get_wtime();
+	pi = (4 * count)/(long double)max;
 	std::cout<<"Sample size is "<<samples<<"."<<std::endl;
 	std::cout<<std::setprecision(12)<<"Approximate value of Pi is "<<pi<<"."<<std::endl;
 	std::cout<<"Time taken is "<<stop - start<<" seconds."<<std::endl;
