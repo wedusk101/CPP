d490fe3687e2241a247f2a5d99e94c73b042bf55
Navonil Mukherjee <navonil_mukherjee@yahoo.co.in>
2020-01-11 21:20:18 +0530

    Cleanup

--- bogoSort.cpp
+++ bogoSort.cpp
@@ -1,14 +1,14 @@
-/*The following code implements the exceedingly inefficient bogo sort algorithm to sort a list of integers in ascending order. It has a factorial class complexity.
-There is a chance that this algorithm may never finish. This is not a bug.*/
+/*The following code implements the exceedingly inefficient bogo sort (also known as slow sort or stupid sort) algorithm to sort a list of integers in ascending order. It has a factorial class complexity.
+Since this is the non-deterministic version of the algorithm, there is a chance that it may never finish. This is not a bug.*/
 #include <iostream>
 #include <random>
 #include <ctime>
 #include <chrono>
 
-void swap(int &, int &);
+void swap(int&, int&);
 void shuffle(int*, int);
-void bogoSort(int *, int);
-bool isSorted(int *, int);
+void bogoSort(int*, int);
+bool isSorted(int*, int);
 
 int main()
 {
@@ -25,7 +25,7 @@ int main()
 	bogoSort(input, size);
 	std::cout << "The sorted list is:" << std::endl;
 	for(int i = 0; i < size; i++)
-			std::cout << input[i] << " ";		
+		std::cout << input[i] << " ";		
 	std::cout << "\n";	
 	auto stop = std::chrono::high_resolution_clock::now();
 	auto diff = std::chrono::duration_cast<std::chrono::seconds>(stop - start);
@@ -57,7 +57,6 @@ void shuffle(int *input, int size) // Knuth-Fisher-Yates shuffle
 
 void bogoSort(int *input, int size) // picks a random permutation of the list and checks if it is in sorted order
 {
-	bool unSorted = true;
 	while(!isSorted(input, size))
 		shuffle(input, size);		
 }
@@ -65,7 +64,7 @@ void bogoSort(int *input, int size) // picks a random permutation of the list an
 bool isSorted(int *input, int size)
 {
 	if (size == 2)
-		return input[0] > input[1] ? false : true;
+		return input[0] > input[1] ? false : true; // when there are only two elements in the list
 	for(int i = 1; i < size - 1; i++)
 		if(!(input[i] >= input[i - 1] && input[i] <= input[i + 1]))
 				return false;
