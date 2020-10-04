43a81d85781e248a5eb35361ab6d996046370d9f
Navonil Mukherjee <navonil_mukherjee@yahoo.co.in>
2020-10-05 03:56:39 +0530

    Update STL_multithreaded_Monte_Carlo_Pi_calc.cpp

--- STL_multithreaded_Monte_Carlo_Pi_calc.cpp
+++ STL_multithreaded_Monte_Carlo_Pi_calc.cpp
@@ -8,33 +8,34 @@ flag -pthread during compilation to link the POSIX thread library pthreads. This
 #include <chrono>
 #include <functional>
 #include <unistd.h>
+#include <cstdint>
 
 #define CACHE_LINE_SIZE 64 // 64 bytes 
-#define PADDING 8 // each row will take up 64 bytes - 8 elements each of 8 bytes [sizeof(long long)]
+#define PADDING 8 // each row will take up 64 bytes - 8 elements each of 8 bytes [sizeof(size_t)]
 
-long long getRandom(long long min, long long max, std::mt19937 &generator)
+size_t getRandom(size_t min, size_t max, std::mt19937 &generator)
 {	
-	std::uniform_int_distribution<long long> distribution(min, max);
+	std::uniform_int_distribution<size_t> distribution(min, max);
 	return distribution(generator);
 }
 
-void calcPi(long long iterations, long long samples, uint32_t numThreads, long long &count)
+void calcPi(size_t iterations, size_t samples, uint32_t numThreads, size_t &count)
 {
 	thread_local std::mt19937 generator;
-	long long maxItr = iterations / numThreads;
-	for(long long i = 0; i < maxItr ; i++) // sampling
+	size_t maxItr = iterations / numThreads;
+	for(size_t i = 0; i < maxItr ; i++) // sampling
 	{	
-		long long x = getRandom(0, std::numeric_limits<unsigned int>::max(), generator) % samples + 1;
-		long long y = getRandom(0, std::numeric_limits<unsigned int>::max(), generator) % samples + 1;
+		size_t x = getRandom(0, std::numeric_limits<unsigned int>::max(), generator) % samples + 1;
+		size_t y = getRandom(0, std::numeric_limits<unsigned int>::max(), generator) % samples + 1;
 			if(x * x + y * y <= iterations) // x^2 + y^2 <= r^2 where r is the radius
 				count++;		
 	}
 }
 
-long double getPi(uint32_t numThreads, long long samples)
+long double getPi(uint32_t numThreads, size_t samples)
 {
-	long long max = samples * samples, sum = 0;
-	long long count[numThreads][PADDING] = {0}; // padding to avoid false sharing
+	size_t max = samples * samples, sum = 0;
+	size_t count[numThreads][PADDING] = {0}; // padding to avoid false sharing
 	std::thread *piThreads = new std::thread[numThreads];
 	
 	for (uint32_t i = 0; i < numThreads; i++)
@@ -50,13 +51,13 @@ long double getPi(uint32_t numThreads, long long samples)
 
 int main() 
 {
-	long long samples = 0; // the value of samples controls the precision of the approximation
+	size_t samples = 0; // the value of samples controls the precision of the approximation
 	long double pi = 0.0;
 	uint32_t numThreads = 0;
 	
 	uint32_t cache_line_size = sysconf(_SC_LEVEL1_DCACHE_LINESIZE); // works for linux - comment out otherwise
 	
-	std::cout << "Size of long long: " << sizeof(long long) << std::endl;
+	std::cout << "Size of size_t: " << sizeof(size_t) << std::endl;
 	std::cout << "CPU cache line size: " << cache_line_size << std::endl; 
 	
 	std::cout<<"Please enter the sample size. Doubling the sample size quadruples the computation time."<<std::endl;
@@ -70,6 +71,6 @@ int main()
 	
 	std::cout<<"Sample size is "<<samples<<"."<<std::endl;
 	std::cout<<std::setprecision(12)<<"Approximate value of Pi is "<<pi<<"." << std::endl;
-	auto diff = std::chrono::duration_cast<std::chrono::seconds>(stop - start);
-	std::cout << "\nTime taken is " << diff.count() << " seconds." << std::endl;
+	auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(stop - start);
+	std::cout << "\nTime taken is " << diff.count() << " milliseconds." << std::endl;
 }
