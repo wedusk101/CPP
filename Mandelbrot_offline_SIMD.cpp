7fead613773c13a6e386cfd002ed5b0ef4e53e8e
Navonil Mukherjee <navonil_mukherjee@yahoo.co.in>
2021-02-20 02:44:08 +0530

    Update Mandelbrot_offline_SIMD.cpp

--- Mandelbrot_offline_SIMD.cpp
+++ Mandelbrot_offline_SIMD.cpp
@@ -222,17 +222,19 @@ void drawMandelbrotSIMD(const int &width, const int &height, int isBenchmark)
 	__m128i _maskwhileitr, _itr, _constmaxitr, _indexi, _inc1i, _const1i;
 	// __m128i _widthi = _mm_setr_epi32(width, width, width, width);
 	
+	// initialize floating point registers
 	_const1neg = _mm_set1_ps(-1.0);
 	_const2 = _mm_set1_ps(2.0);
 	_const4 = _mm_set1_ps(4.0);
 	_const2p5neg = _mm_set1_ps(-2.5);
 	
+	_invw = _mm_set1_ps(invW);
+	_invh = _mm_set1_ps(invH);	
+	
+	// initialize integer registers
 	 _constmaxitr = _mm_set1_epi32(MAX_ITR);
 	_itr = _mm_set1_epi32(0);
-	_const1i = _mm_set1_epi32(1);
-	
-	_invw = _mm_set1_ps(invW);
-	_invh = _mm_set1_ps(invH);
+	_const1i = _mm_set1_epi32(1);	
 	
 	
 	for (int y = 0; y < height; y++) // y axis of the image	
@@ -259,8 +261,13 @@ void drawMandelbrotSIMD(const int &width, const int &height, int isBenchmark)
 			
 			_indexi = _mm_setr_epi32(index3, index2, index1, index0); // store set of 4 packed consecutive indices
 			
-			int itr = 0;			
-			float zr = 0, zi = 0, cr = 0, ci = 0;	
+			int itr = 0;	
+			
+			float zr = 0, zi = 0, cr = 0, ci = 0;				
+			_zr = _mm_set1_ps(0);
+			_zi = _mm_set1_ps(0);
+			_cr = _mm_set1_ps(0);
+			_ci = _mm_set1_ps(0);
 			
 			// cr = (x * invW) - 2.5;			
 			// _cr =  _mm_fmadd_ps(_xf, _invw, _const2p5neg); // No FMA on my Nehalem CPU			 
@@ -276,6 +283,8 @@ void drawMandelbrotSIMD(const int &width, const int &height, int isBenchmark)
 			_zr2 = _mm_mul_ps(_zr, _zr); // zr * zr
 			_zi2 = _mm_mul_ps(_zi, _zi); // zi * zi
 			_mod = _mm_add_ps(_zr2, _zi2); // zr * zr + zi * zi
+			
+			loop: // while(...)
 			
 			_maskwhile = _mm_cmple_ps(_mod, _const4); // zr * zr + zi * zi <= 4.0
 			_maskwhileitr = _mm_cmplt_epi32(_itr, _constmaxitr); // itr < MAX_ITR			
@@ -297,9 +306,15 @@ void drawMandelbrotSIMD(const int &width, const int &height, int isBenchmark)
 				_zi = _mm_add_ps(_zi, _ci);
 				
 
-				_inc1i = _mm_and_ps(_maskwhile, _inc)
+				_inc1i = _mm_and_ps(_maskwhile, _inc1i)
 				// _itr
 			}
+			
+			// if (any one register satisfies while condition)
+				// goto loop;
+			
+			
+			
 			if (itr < MAX_ITR)
 				frameBuffer[index] = BLACK;
 			else
