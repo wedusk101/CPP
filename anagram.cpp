24050122f7962217f278eef1913f083792cec71a
Navonil Mukherjee <navonil_mukherjee@yahoo.co.in>
2022-09-23 18:58:11 +0530

    Added docs
    Also fixed undefined behavior (sort of)

--- anagram.cpp
+++ anagram.cpp
@@ -1,12 +1,36 @@
-/* The following code uses the fundamental theorem of arithmetic to check if two pieces of text
-are anagrams of each other. Please note that the program is case sensitive.*/
+/* 
+
+The following code uses the fundamental theorem of arithmetic to check if two pieces of 
+text are anagrams of each other. Anagrams are strings made of the same set of characters.
+For example, "Dirty room" and "Dormitory" are anagrams. Please note that the program is
+NOT case sensitive to input and ignores spaces in the strings. 
+
+Please visit https://wordsmith.org/anagram/hof.html for a list of anagrams.
+
+The algorithm works by generating a lookup table (LUT) of the letters of the alphabet and
+their corresponding prime number values. For example, a = 2, b = 3, c = 5, d = 7 etc.
+The LUT we generate spans the basic ASCII space from 0 through 127. This LUT is then used
+to read the prime number values of the corresponding letters in the input string. 
+The resulting numbers are then multiplied and stored in a variable. The same operation is
+performed for the other input string. The resulting products are compared. This comparison
+will return true if and only if the two input strings are anagrams. This is due to the 
+properties of the fundamental tehorem of arithmetic which states that any composite
+number can be factored into one one set of prime factors.
+
+NOTE: This program relies on unsigned integer overflow which is defined behavior per the 
+C++ standard.
+
+*/
+
 #include <iostream>
 #include <cmath>
+#include <cstddef>
+#include <algorithm>
 
-void initLUT(int*, const int &);
-bool isAnagram(const std::string &, const std::string &, int*);
-bool isPrime(int);
-int getNextPrime(int &);
+void initLUT(size_t*, const size_t &);
+bool isAnagram(const std::string &, const std::string &, size_t*);
+bool isPrime(size_t);
+size_t getNextPrime(size_t &);
 
 int main()
 {
@@ -16,8 +40,16 @@ int main()
 	getline(std::cin, first);
 	std::cout << "Please enter the second string." << std::endl;
 	getline(std::cin, second);
-	std::cout << "The input strings are: \n" << first << " - " << second << std::endl;
-	int *map = new int[128];
+	std::cout << "\nThe input strings are: \n" << first << " - " << second << std::endl;
+	size_t *map = new size_t[128];
+	
+	// convert strings to lower case
+	std::transform(first.begin(), first.end(), first.begin(),
+    [](unsigned char c){ return std::tolower(c); });
+	
+	std::transform(second.begin(), second.end(), second.begin(),
+    [](unsigned char c){ return std::tolower(c); });
+	
 	initLUT(map, 128);
 	if(isAnagram(first, second, map))
 		std::cout << "The two strings are anagrams." << std::endl;
@@ -26,9 +58,9 @@ int main()
 	delete [] map;
 }
 
-bool isPrime(int n)
+bool isPrime(size_t n)
 {
-	int i = 0;
+	size_t i = 0;
 	if(n <= 1)
 		return false;
 	else
@@ -38,27 +70,28 @@ bool isPrime(int n)
 	return true;
 }
 
-int getNextPrime(int &p) 
+size_t getNextPrime(size_t &p) 
 {
 	for( p += 1; ; p++)
 		if(isPrime(p))
 			return p;
 }
 
-void initLUT(int *map, const int &size) // initializes the lookup table with consecutive primes
+// initializes the lookup table with consecutive primes
+void initLUT(size_t *map, const size_t &size) 
 {
-	int j = 1;
+	size_t j = 1;
 	for(int i = 0; i < size; i++)
 		map[i] = getNextPrime(j);
 	map[32] = 1; // we ignore spaces
 }
 
-bool isAnagram(const std::string &first, const std::string &second, int *lookup)
+bool isAnagram(const std::string &first, const std::string &second, size_t *lookup)
 {
-	int firstProduct = 1, secondProduct = 1;
-	for(int i = 0; i < first.size(); i++)
+	size_t firstProduct = 1, secondProduct = 1;
+	for(size_t i = 0; i < first.size(); i++)
 		firstProduct *= lookup[(int)first[i]];
-	for(int i = 0; i < second.size(); i++)
-		secondProduct *= lookup[(int)second[i]];
+	for(size_t i = 0; i < second.size(); i++)
+		secondProduct *= lookup[(size_t)second[i]];
 	return (firstProduct == secondProduct);
 }
